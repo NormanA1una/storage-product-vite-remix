9780efa1db9734a091467f43a8ba5c37a4400792
NormanA1una <baciloferoz@gmail.com>
2024-11-25 16:12:37 -0600

    del: redux provider in main-layout.tsx

--- app/layouts/main/main-layout.tsx
+++ app/layouts/main/main-layout.tsx
@@ -1,9 +1,6 @@
 import { Outlet } from "@remix-run/react";
 import { Navbar } from "../navbar";
 import { Footer } from "../footer";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "~/store/store";
 
 const CONTACT_MEDIA: ContactMedia = [
   {
@@ -32,17 +29,13 @@ export default function MainLayout({
 }: MainLayoutProps) {
   return (
     <>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Navbar
-            pathNames={pathNames}
-            contactMedia={CONTACT_MEDIA}
-            phoneNumber={phoneNumber}
-          />
-          <Outlet context={{ phoneNumber }} />
-          <Footer pathNames={pathNames} contactMedia={CONTACT_MEDIA} />
-        </PersistGate>
-      </Provider>
+      <Navbar
+        pathNames={pathNames}
+        contactMedia={CONTACT_MEDIA}
+        phoneNumber={phoneNumber}
+      />
+      <Outlet context={{ phoneNumber }} />
+      <Footer pathNames={pathNames} contactMedia={CONTACT_MEDIA} />
     </>
   );
 }
